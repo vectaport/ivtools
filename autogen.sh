ec0401e83902c0128c51b028436ed196d8838823
Barak A. Pearlmutter <barak+git@pearlmutter.net>
2015-05-19 08:35:46 +0100

    tweak autogen.sh

--- autogen.sh
+++ autogen.sh
@@ -2,14 +2,16 @@
 
 set -e
 
-## option --foreign engaged so these files are no longer mandatory
+# echo "Usage: ./autoget.sh [options]"
+# echo "Options:"
+# echo "  --warn=all    warn about various autoconf constructs"
+
+## automake --foreign option engaged so these files are no longer mandatory:
 # for f in NEWS AUTHORS ChangeLog; do
 #     test -e "$f" || cp --verbose /dev/null "$f"
 # done
 
-mkdir --parent --verbose m4
+## Unnecessary: m4/acltx_prog_ps2pdf.m4 and hence m4/ now in the repo.
+# mkdir --parent --verbose m4
 
-autoreconf --install --symlink
-
-## Use --force to recreate things that don't seem to require recreation:
-# autoreconf --install --symlink --make --force
+autoreconf --force --install --symlink $*
