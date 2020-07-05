f4db154500450fe2fdf02a42fa630af630879b80
Scott Johnston <johnston@vectaport.com>
2020-07-05 09:46:19 -0500

    release ivtools-2.0.2

--- src/include/ivstd/version.h
+++ src/include/ivstd/version.h
@@ -1,3 +1,3 @@
-#define IvtoolsVersion 2.0.1
-#define VersionString "2.0.1"
-#define ReleaseString "ivtools-2.0.1"
+#define IvtoolsVersion 2.0.2
+#define VersionString "2.0.2"
+#define ReleaseString "ivtools-2.0.2"
