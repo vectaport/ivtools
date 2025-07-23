d6ffc7bcedbc96eea41af73fccbf5975151b5dbb
John Carlson <yottzumm@gmail.com>
2025-07-23 01:46:49 -0500

    Changed Linux return value

--- src/TIFF/tiffcompat.h
+++ src/TIFF/tiffcompat.h
@@ -153,7 +153,7 @@ extern	void TIFFUnmapFileContents();
 extern long mpw_lseek(int, long, int);
 #else
 #ifdef __linux__
-extern	long lseek(int fd, off_t offset, int whence);
+extern	off_t lseek(int fd, off_t offset, int whence);
 #else
 #if !defined(__NetBSD__) && !defined(__FreeBSD__)
 extern	long lseek();
