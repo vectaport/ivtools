baabf39bceadde44a4a374af86b850c1b5f29913
Scott Johnston <johnston@vectaport.com>
2026-09-16 07:52:27 +0200

    Update patch.h

--- src/include/ivstd/patch.h
+++ src/include/ivstd/patch.h
@@ -18,6 +18,6 @@
    the change lands, which is what makes a PATCH_KEY later resolve back
    to the commit it named. A PR that only bumps this value needs no
    separate tagging step and no tag-push commit. */
-#define PATCH_KEY "5360fe20"
+#define PATCH_KEY "360fe205"
 
 #endif /* _patch_h */
